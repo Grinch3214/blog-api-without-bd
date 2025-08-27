b77dfe3c15c9aefac4ce90f911a175cb82e622aa
Max <scorpiomax90@gmail.com>
2025-08-27 22:51:06 +0300

    Create backend and fronted for simple test api

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ result }}
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const result = ref([]);
+
+async function getData() {
+  const url = 'http://127.0.0.1:5000/';
+  try {
+    const response = await axios.get(url);
+    result.value = response.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+onMounted(async () => {
+  await getData();
+});
+</script>
