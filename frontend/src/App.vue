133a74e2952073055f288aa321114eb89fd54b2e
Max <scorpiomax90@gmail.com>
2025-08-28 00:07:25 +0300

    Added tailwind v.4 on frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <nav>
+  <nav class="flex text-3xl gap-5 font-bold justify-center text-blue-900">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/new-post">New Post page</RouterLink>
   </nav>
 
-  <RouterView />
+  <main class="container mx-auto px-4">
+    <RouterView />
+  </main>
 </template>
 
 <script setup>
